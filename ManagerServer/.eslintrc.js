5a2a79b98ed7ee679b021f88d087d2982380b0be
petrubear <edison.martinez@gmail.com>
2021-08-07 12:59:02 -0500

    feat: add eslint config

---
+++ ManagerServer/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es2021': true,
+    },
+    'extends': [
+        'google',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 12,
+        'sourceType': 'module',
+    },
+    'plugins': [
+        '@typescript-eslint',
+    ],
+    'rules': {
+        'indent': ['error', 4],
+        'max-len': ['error', {'code': 120}],
+        'require-jsdoc': ['off'],
+        'space-before-function-paren': ['error', {
+            'anonymous': 'always',
+            'named': 'never',
+            'asyncArrow': 'always',
+        }],
+    },
+};

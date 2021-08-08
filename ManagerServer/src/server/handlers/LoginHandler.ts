70edb4ff5fdd2655a18670fc70ba760851f4b171
petrubear <edison.martinez@gmail.com>
2021-08-08 17:28:48 -0500

    feat: basic http codes and methods

--- ManagerServer/src/server/handlers/LoginHandler.ts
+++ ManagerServer/src/server/handlers/LoginHandler.ts
@@ -1,5 +1,6 @@
 import {IncomingMessage, ServerResponse} from 'http';
 import {Account, Handler, TokenGenerator} from '../model/Models';
+import {HTTP_CODES, HTTP_METHODS} from '../../shared/Model';
 
 export class LoginHandler implements Handler {
     private req: IncomingMessage;
@@ -13,13 +14,34 @@ export class LoginHandler implements Handler {
     }
 
     public async handleRequest(): Promise<void> {
+        switch (this.req.method) {
+            case HTTP_METHODS.POST:
+                await this.handlePost();
+                break;
+            default:
+                await this.handleNotFound();
+                break;
+        }
+    }
+
+    private async handleNotFound(): Promise<void> {
+        this.res.statusCode = HTTP_CODES.NOT_FOUND;
+        this.res.write('not found');
+    }
+
+    private async handlePost(): Promise<void> {
         try {
             const body = await this.getRequestBody();
             const sessionToken = await this.tokenGenerator.generateToken(body);
             if (sessionToken) {
-                this.res.write('valid credentials');
+                this.res.statusCode = HTTP_CODES.CREATED;
+                this.res.writeHead(HTTP_CODES.CREATED, {
+                    'Content-Type': 'application/json',
+                });
+                this.res.write(JSON.stringify(sessionToken));
             } else {
-                this.res.write('wrong credentials');
+                this.res.statusCode = HTTP_CODES.NOT_FOUND;
+                this.res.write('wrong username or password');
             }
         } catch (error) {
             this.res.write('error:' + error.message);
